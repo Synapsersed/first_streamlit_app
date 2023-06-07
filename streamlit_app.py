2882cd1aee98362f7dd9b082ec24960728bd9455
Synapsersed <55678371+Synapsersed@users.noreply.github.com>
2023-06-07 11:27:56 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,4 +24,7 @@ streamlit.dataframe(fruits_to_show)
 # New section to display fruityvice API response
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.header("Fruityvice Fruit Advice!")
 streamlit.text(fruityvice_response)
+
+
